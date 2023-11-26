9b37f7bb3207c44c89b348fb23413abec62631ad
HaiquanZ <nn764562@gmail.com>
2023-11-26 20:29:29 +0700

    update class

--- frontend/src/app/service/class.service.ts
+++ frontend/src/app/service/class.service.ts
@@ -63,4 +63,25 @@ export class ClassService {
       }
     )
   }
+
+  edit(data: any, callBack?: Function, option?: any): any {
+    this.http.put(baseUrl + `admin/class/${option}`, data, { observe: 'response', headers: this.headers }).subscribe(
+      response => {
+        if (response.body) {
+          let body: any = Object.assign({}, response.body);
+          if (body) {
+            if (callBack) {
+              callBack(response);
+            }
+          }
+        }
+      },
+      error => {
+        if (callBack) {
+          callBack(null);
+          this.alertSrv.showError('Something went wrong', 'Lỗi!');
+        }
+      }
+    )
+  }
 }
